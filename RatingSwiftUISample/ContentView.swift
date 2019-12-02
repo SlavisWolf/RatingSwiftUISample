ace4be2caa94bfd96bcb432e86def5d93fcf61f3
Antonio <antoniojesus.ib@gmail.com>
2019-12-02 10:02:33 +0100

    Add files via upload

---
+++ RatingSwiftUISample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RatingSwiftUISample
+//
+//  Created by Antonio Jesús on 02/12/2019.
+//  Copyright © 2019 Antonio Jesús. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
